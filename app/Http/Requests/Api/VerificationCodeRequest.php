39659c1a7866a5c505efab1ff4ffc153c630c471
hycnsz <1958006691@qq.com>
2019-11-27 20:02:13 +0800

    '发送短信验证码'

---
+++ app/Http/Requests/Api/VerificationCodeRequest.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Requests\Api;
+
+class VerificationCodeRequest extends FormRequest
+{
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    
+    public function rules()
+    {
+        return [
+
+            'phone' => [
+                'required',
+                'regex:/^((13[0-9])|(14[5,7])|(15[0-3,5-9])|(17[0,3,5-8])|(18[0-9])|166|198|199)\d{8}$/',
+                'unique:users'
+            ]
+        ];
+        if (!app()->environment('production')) {
+            $code = '1234';
+        } else {
+            // 生成4位随机数，左侧补0
+            $code = str_pad(random_int(1, 9999), 4, 0, STR_PAD_LEFT);
+
+            try {
+                $result = $easySms->send($phone, [
+                    'template' => config('easysms.gateways.aliyun.templates.register'),
+                    'data' => [
+                        'code' => $code
+                    ],
+                ]);
+            } catch (\Overtrue\EasySms\Exceptions\NoGatewayAvailableException $exception) {
+                $message = $exception->getException('aliyun')->getMessage();
+                abort(500, $message ?: '短信发送异常');
+            }
+        }
+    }
+    
+}
